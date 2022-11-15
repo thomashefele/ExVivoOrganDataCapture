3285e2951db484e04bfb1f1fa7e6512dd6f542c8
thomashefele <116929892+thomashefele@users.noreply.github.com>
2022-11-15 14:32:22 -0500

    Update sensors.py

--- sensors.py
+++ sensors.py
@@ -1,86 +1,135 @@
+#individual sensor test
+
 import serial as ser
 import time
+import numpy as np
+from math import floor
+from threading import Thread
+import pandas as pd
 
-s = ser.Serial("COM4", 2400, timeout=1000)
-data_KM = []
-time_FT_1 = []
-
+#---------------------------
+#MedTronic sensor
+        
+lap = 10
 start = time.time()
 
-i = 1
-Sum = 0
+MT_port = ser.serial("COM3", 9600, timeout= 1000)
+MT_port.write(b"DR 01 0137\r")
+data_AF, data_AP = [],[]
+AF_avg, AP_avg, ts_MT = [],[],[]
+
 while True:
-    global time_FT_1
-    end_FT_1 = time.ctime()
-    lap_FT_1 = round(time.time() - start)
-    time_FT_1.append(end_FT_1)
-    FT_1_byte = s.read(6)
-    FT_1_str = str(FT_1_byte)
-    if lap_FT_1%5 == 0:
-        avg = Sum/i
-        print(f"{avg} {end_FT_1}")
-        Sum = 0
-        i = 1
-    else:
-        Sum += float(FT_1_str[2:8])
+        intv = round(time.time() - start)
+        MT_str = str(MT_port.read(35))
+        AF_str = MT_str[5:8] + "." + MT_str[8:10]
+        AP_str = MT_str[11:15]
+
+        if MT_str[5] == "+" and MT_str[11] == "+":
+            data_AF.append(float(AF_str[1:5]))
+            data_AP.append(float(AP_str[12:15]))
+        elif MT_str[5] == "+" and MT_str[11] == "+":
+            data_AF.append(float(AF_str[1:5]))
+            data_AP.append(float(AP_str[11:15]))
+        elif MT_str[5] != "+" and MT_str[11] == "+":
+            data_AF.append(float(AF_str[0:5]))
+            data_AP.append(float(AP_str[12:15]))
+        else:
+            data_AF.append(float(AF_str[0:5]))
+            data_AP.append(float(AP_str))
+
+        if intv%interval == 0:
+            AF_avg.append(np.mean(data_AF))
+            AP_avg.append(np.mean(data_AP))
+            ts_MT.append(intv)
+            data_AF, data_AP = [],[]
+        else:
+            pass
+        
+print(AF_avg, AP_avg, ts_MT)
+
+#---------------------------
+#kidney mass - force transducer
+
+lap = 10
+start = time.time()
+
+FT_1_port = ser.serial("COM4", 2400, timeout= 10000)
+data_FT_1 = []
+i = 0
+FT_1_avg, ts_FT_1 = [], []
+
+while True:
+        intv = round(time.time() - start)           
+        FT_1_str = str(FT_1_port.read(6))
+        data_FT_1.append(float(FT_1_str[2:8])
+
+        if intv != 0 and intv%interval == 0 and i%(interval - 1) == 0:
+               FT_1_avg.append(np.mean(data_FT_1))
+               ts_FT_1.append(intv)
+               data_FT_1 = []
+               i = 0
+        else:
+               pass
         i += 1
-    global data_KM
-    data_KM.append(FT_1_str)
-    
-    #------------------------------------------------------------------------------
-    
-    #necessary libraries
-import serial as ser
-import threading
-import numpy as np
-import time
-import pandas as pd
-import matplotlib as plt
+                         
+print(FT_1_avg, ts_FT_1)
+                         
+#---------------------------
+#BioTrend - sensor
 
-MT = ser.Serial("COM3", 9600, timeout=1000)
-MT.write(b'DR 05 013B\r')
-time_MT = []
-data_AF = []
-data_AP = []
+lap = 10
+start = time.time()
+
+BT_port = ser.serial("COM5", 9600, timeout= 1000)
+N = 1
+data_sO2v, data_hct = [],[]                               
+sO2v_avg, hct_avg, ts_BT = [],[],[]
+while True:
+        intv = floor(time.time() - start)
+        mod = intv%interval 
+        BT_str = str(BT_port.read(43))
+        #change below when data is available during trial perfusion
+        #data_sO2v.append(BT_str[12:14]))
+        #data_hct.append(BT_str[20:22]))
+        #change "0" to data_...[-1] during trial perfusion 
+        if BT_str[12:14] == "--" and BT_str[20:22] == "--":
+                data_sO2v.append(0)
+                data_hct.append(0)               
+        if mod >= 5 and mod < 10:
+               sO2v_avg.append(np.mean(data_sO2V))
+               hct_avg.append(np.mean(data_hct))         
+               ts_BT.append(10*N)
+               data_FT_1 = []
+               N += 1
+        else:
+                pass
+
+print(sO2v_avg, hct_avg, ts_BT)                         
+                         
+#---------------------------
+#urine output - force transducer
+                         
+lap = 10
+start = time.time()
+                         
+
+FT_2_port = ser.serial("COM6", 2400, timeout= 1000)
+data_FT_2 = []
+i = 0
+FT_2_avg, ts_FT_2 = [],[]
 
 while True:
-    global time_MT
-    end_MT = time.ctime()
-    time_MT.append(end_MT)
-    MT_byte = MT.read(35)
-    MT_str = str(MT_byte)
-    AF_str = MT_str[5:8] + "." + MT_str[8:10]
-    AP_str = MT_str[11:15]
-    global data_AF, data_AP
+        intv = round(time.time() - start)           
+        FT_2_str = str(FT_2_port.read(6))
+        data_FT_2.append(float(FT_2_str[2:8])
 
-    if MT_str[5] == "+" and MT_str[11] == "+":
-        data_AF.append(float(AF_str[1:5]))
-        data_AP.append(float(AP_str[12:15]))
-    elif MT_str[5] == "+" and MT_str[11] == "+":
-        data_AF.append(float(AF_str[1:5]))
-        data_AP.append(float(AP_str[11:15]))
-    elif MT_str[5] != "+" and MT_str[11] == "+":
-        data_AF.append(float(AF_str[0:5]))
-        data_AP.append(float(AP_str[12:15]))
-    else:
-        data_AF.append(float(AF_str[0:5]))
-        data_AP.append(float(AP_str))
-    print(f"{AF_str} {AP_str} {end_MT}")
-    
-    #-----------------------------------------------------------------------------
-    
-import serial as ser
-import time
+        if intv != 0 and intv%interval == 0 and i%(interval - 1) == 0:
+               FT_2_avg.append(np.mean(data_FT_2))
+               ts_FT_2.append(time.ctime())
+               data_FT_2 = []
+               i = 0
+        else:
+               pass
+        i += 1
 
-s = ser.Serial("COM6", 9600, timeout=1000)
-
-while True:
-    global time_BT
-    end_BT = time.ctime()
-    time_BT.append(end_BT)
-    BT_byte = s.read(43)
-    BT_str = str(BT_byte)
-    global data_sO2v, data_hct
-    data_sO2v.append(BT_str[2:14])
-    data_hct.append(BT_str[15:22])
-    print(f"{FT_1_str[2:22]} {end_BT}")
+print(FT_2_avg, ts_FT_2)                         
