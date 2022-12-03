a7f33f36ccbc432b38b252bfc21f55b0a985e45c
thomashefele <116929892+thomashefele@users.noreply.github.com>
2022-12-02 21:29:41 -0500

    Update Chem_analysis.py

--- Chem_analysis.py
+++ Chem_analysis.py
@@ -1,4 +1,5 @@
-from time import time
+from os import system, name
+from time import time, asctime
 import pyodbc
 
 #establish database connection
@@ -7,40 +8,64 @@ server = "dtk-server.database.windows.net"
 database = "perf-data"
 username = "dtk_lab"
 password = "data-collection1"
+
+#define function for inputting data and re-prompting if data is invalid
+def data_input(key):
+	try:
+		var = float(input(key))
+	except:
+		while 
+			print("Invalid data type. Please re-enter with a float data type")
+			var = float(input(key))
+	return var
+
 with pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password) as cnxn:
         with cnxn.cursor() as cursor:
+		
+		new_input = "Y"
+		
 		while (new_input == "Y"):
-			time_stamp = time()
+			time_stamp = asctime(time())
 			#iStat measurements
 			print("iStat Measurements:\n)
-			pH = float(input("pH: "))
-			PCO2 = float(input("PCO_2: "))
-			PO2 = float(input("PO_2: "))
-			TCO2_iStat = float(input("TCO_2: "))
-			HCO3 = float(input("HCO_3: "))
-			BE = float(input("BE: "))
-			sO2 = float(input("sO2: "))
-			Hb = float(input("Hb: "))		
+			pH = data_input("pH: ")
+			PCO2 = data_input("PCO_2: ")
+			PO2 = data_input("PO_2: ")
+			TCO2_iStat = data_input("TCO_2: ")
+			HCO3 = data_input("HCO_3: ")
+			BE = data_input("BE: ")
+			sO2 = data_input("sO2: ")
+			Hb = data_input("Hb: ")		
 			cursor.execute(f"INSERT INTO dbo.istat_t([UNOS_ID], [time_stamp], [ph], [pco2], [tco2], [hco3], [be], [so2], [hb]) VALUES({unos_id}, {time_stamp}, {pH}, {PCO2}, {PO2}, {TCO2_iStat}, {HCO3}, {BE}, {sO2}, {Hb}))
 			#Piccolo measurements
 			print("Piccolo Measurements:\n")
-			Na = float(input("Na: "))
-			K = float(input("K: "))
-			TCO2_Pic = float(input("TCO_2: "))
-			Cl = float(input("Cl: "))
-			Glu = float(input("Glu: "))
-			Ca = float(input("Ca: "))
-			BUN = float(input("BUN: "))
-			Cre = float(input("Cre: "))
-			eGFR = float(input("eGFR: "))
-			ALP = float(input("ALP: "))
-			AST = float(input("AST: "))
-			TBIL = float(input("TBIL: "))
-			ALB = float(input("ALB: "))
-		     	TP = float(input("TB: "))
+			Na = data_input("Na: ")
+			K = data_input("K: ")
+			TCO2_Pic = data_input("TCO_2: ")
+			Cl = data_input("Cl: ")
+			Glu = data_input("Glu: ")
+			Ca = data_input("Ca: ")
+			BUN = data_input("BUN: ")
+			Cre = data_input("Cre: ")
+			eGFR = data_input("eGFR: ")
+			ALP = data_input("ALP: ")
+			AST = data_input("AST: ")
+			TBIL = data_input("TBIL: ")
+			ALB = data_input("ALB: ")
+		     	TP = data_input("TB: ")
 	     		cursor.execute(f"INSERT INTO dbo.pic_t([UNOS_ID], [time_stamp], [Na], [K], [tco2], [Cl], [glu], [Ca], [BUN], [cre], [egfr], [alp], [ast], [tbil], [alb], [tp]) VALUES({unos_id}, {time_stamp}, {Na}, {K}, {TCO2_Pic}, {Cl}, {Glu}, {Ca}, {BUN}, {Cre}, {eGFR}, {ALP}, {AST}, {TBIL}, {ALB}, {TP}))
 		     	#conditional for executing the loop again
-		     	new_input = input("Enter a new set of data? (Y/N)")
+		     	new_input = input("Enter a new set of data? (Y/N) \n")
+		       
+		        while (new_input != "Y" or new_input != "N"):
+				print("Invalid entry. Try again.\n")
+			       	new_input = input("Enter a new set of data? (Y/N) ")
+				       
+		       if (new_input == "Y"):
+				system("cls")
+		       else:
+				pass
+			       	
 		       	
 	     	
 	     	
