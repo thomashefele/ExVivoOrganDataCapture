1fcf9cbc6becae2e7f7efcd2a709396608f49d00
thomashefele <116929892+thomashefele@users.noreply.github.com>
2022-12-05 17:01:43 -0500

    Update Chem_analysis.py

--- Chem_analysis.py
+++ Chem_analysis.py
@@ -46,7 +46,7 @@ while (new_input == "Y"):
     BE = data_input("BE: ")
     sO2 = data_input("sO2: ")
     Hb = data_input("Hb: ")
-    cursor.execute(f"INSERT INTO dbo.istat_t([UNOS_ID], [time_stamp], [ph], [pco2], [tco2], [hco3], [be], [so2], [hb]) VALUES({unos_id}, {time_stamp}, {pH}, {PCO2}, {PO2}, {TCO2_iStat}, {HCO3}, {BE}, {sO2}, {Hb}))
+    cnxn.execute(f"INSERT INTO dbo.istat_t([UNOS_ID], [time_stamp], [ph], [pco2], [tco2], [hco3], [be], [so2], [hb]) VALUES({unos_id}, {time_stamp}, {pH}, {PCO2}, {PO2}, {TCO2_iStat}, {HCO3}, {BE}, {sO2}, {Hb}))
     #Piccolo measurements
     print("Piccolo Measurements:")
     sleep(1)
@@ -64,7 +64,7 @@ while (new_input == "Y"):
     TBIL = data_input("TBIL: ")
     ALB = data_input("ALB: ")
     TP = data_input("TB: ")
-    cursor.execute(f"INSERT INTO dbo.pic_t([UNOS_ID], [time_stamp], [Na], [K], [tco2], [Cl], [glu], [Ca], [BUN], [cre], [egfr], [alp], [ast], [tbil], [alb], [tp]) VALUES({unos_id}, {time_stamp}, {Na}, {K}, {TCO2_Pic}, {Cl}, {Glu}, {Ca}, {BUN}, {Cre}, {eGFR}, {ALP}, {AST}, {TBIL}, {ALB}, {TP}))
+    cnxn.execute(f"INSERT INTO dbo.pic_t([UNOS_ID], [time_stamp], [Na], [K], [tco2], [Cl], [glu], [Ca], [BUN], [cre], [egfr], [alp], [ast], [tbil], [alb], [tp]) VALUES({unos_id}, {time_stamp}, {Na}, {K}, {TCO2_Pic}, {Cl}, {Glu}, {Ca}, {BUN}, {Cre}, {eGFR}, {ALP}, {AST}, {TBIL}, {ALB}, {TP}))
     #conditional for executing the loop again
     new_input = input("Enter a new set of data? (Y/N) ")
 
