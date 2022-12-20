4af40769a4b6dabad471c5cfa60952895a3d216a
thomashefele <116929892+thomashefele@users.noreply.github.com>
2022-12-20 13:56:15 -0500

    Update rando_data.py

--- rando_data.py
+++ rando_data.py
@@ -26,26 +26,26 @@ cursor_FT2 = cnxn_FT2.cursor()
 #data generator
 while lap <= interval:
  
-  unos_id = 1234
+  unos_id = "1234"
   ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
   
   #MedTronic data
   data_AF = round(rand.random(), 3)
   data_AP = round(5*rand.random(), 3)
-  cursor_MT.execute(f"INSERT INTO dbo.mt_t([UNOS_ID], [time_stamp], [flow], [pressure]) VALUES({unos_id}, '{ts}', {data_AF}, {data_AP});")
+  cursor_MT.execute(f"INSERT INTO dbo.mt_t([UNOS_ID], [time_stamp], [flow], [pressure]) VALUES('{unos_id}', '{ts}', {data_AF}, {data_AP});")
   cnxn_MT.commit()
   #Kidney mass data
   data_KM = round(5*rand.random(), 3)
-  cursor_FT1.execute(f"INSERT INTO dbo.km_t([UNOS_ID], [time_stamp], [kidney_mass]) VALUES({unos_id}, '{ts}', {data_KM});")
+  cursor_FT1.execute(f"INSERT INTO dbo.km_t([UNOS_ID], [time_stamp], [kidney_mass]) VALUES('{unos_id}', '{ts}', {data_KM});")
   cnxn_FT1.commit()
   #Urine output data
   data_UO = round(5*rand.random(), 3)
-  cursor_FT2.execute(f"INSERT INTO dbo.uo_t([UNOS_ID], [time_stamp], [urine_output]) VALUES({unos_id}, '{ts}', {data_UO});")
+  cursor_FT2.execute(f"INSERT INTO dbo.uo_t([UNOS_ID], [time_stamp], [urine_output]) VALUES('{unos_id}', '{ts}', {data_UO});")
   cnxn_FT2.commit()
   #Biotrend data
   data_sO2 = round(100*rand.random(), 3)
   data_hct = round(30*rand.random(), 3)
-  cursor_BT.execute(f"INSERT INTO dbo.bt_t([UNOS_ID], [time_stamp], [sO2], [hct]) VALUES({unos_id}, '{ts}', {data_sO2}, {data_hct});")
+  cursor_BT.execute(f"INSERT INTO dbo.bt_t([UNOS_ID], [time_stamp], [sO2], [hct]) VALUES('{unos_id}', '{ts}', {data_sO2}, {data_hct});")
   cnxn_BT.commit()
   
   sleep(5)
