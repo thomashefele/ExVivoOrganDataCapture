3c4fe0ec23156d9a549fcd9fa4702ced2fac2750
thomashefele <116929892+thomashefele@users.noreply.github.com>
2023-01-24 12:51:32 -0500

    Update rando_data.py

--- rando_data.py
+++ rando_data.py
@@ -1,5 +1,6 @@
-from pyodbcm import platform
+import pyodbc, platform
 import random as rand
+from time import time, sleep
 
 #timer
 start = time() 
@@ -34,22 +35,12 @@ cursor_FT1 = cnxn_FT1.cursor()
 cnxn_FT2 =  pyodbc.connect(connString)
 cursor_FT2 = cnxn_FT2.cursor()
 
-unos_id = "TEST_ID_1"
-eth, gender, bt, age, bmi, weight = "Hispanic", "M", "O", "37", "23.4", "190"
-cursor_don.execute("INSERT INTO dbo.organ_t([UNOS_ID], [time_stamp], [blood_type], [weight], [age], [bmi], [gender], [eth_race]) VALUES('{}', GETDATE(), '{}', '{}', '{}', '{}', '{}', '{}');".format(unos_id, bt, weight, age, bmi, gender, eth))
-cnxn_don.commit()
-
-unos_id = "TEST_ID_2"
-eth, gender, bt, age, bmi, weight = "White", "F", "A", "24", "20.6", "155"
-cursor_don.execute("INSERT INTO dbo.organ_t([UNOS_ID], [time_stamp], [blood_type], [weight], [age], [bmi], [gender], [eth_race]) VALUES('{}', GETDATE(), '{}', '{}', '{}', '{}', '{}', '{}');".format(unos_id, bt, weight, age, bmi, gender, eth))
-cnxn_don.commit()
-
 i = 0
 
 #data generator
 while lap <= interval:
     
-  if lap < 1800:
+  if lap < (interval/2):
         unos_id = "TEST_ID_1"
         
         if i == 0:
@@ -58,7 +49,7 @@ while lap <= interval:
             cnxn_don.commit()
             i = 1
     
-  elif lap >= 1800:
+  elif lap >= (interval/2):
         unos_id = "TEST_ID_2"
         
         if i == 1:
@@ -70,7 +61,8 @@ while lap <= interval:
   #MedTronic data
   data_AF = round(rand.random(), 3)
   data_AP = round(5*rand.random(), 3)
-  cursor_MT.execute("INSERT INTO dbo.mt_t([UNOS_ID], [time_stamp], [flow], [pressure]) VALUES('{}', GETDATE(), {}, {});".format(unos_id, data_AF, data_AP)))
+  rpm = round(5000*rand.random())
+  cursor_MT.execute("INSERT INTO dbo.mt_t([UNOS_ID], [time_stamp], [flow], [pressure], [rpm]) VALUES('{}', GETDATE(), {}, {}, {});".format(unos_id, data_AF, data_AP, rpm))
   cnxn_MT.commit()
   #Kidney mass data
   data_KM = round(5*rand.random(), 3)
@@ -78,10 +70,10 @@ while lap <= interval:
   cnxn_FT1.commit()
   #Urine output data
   data_UO = round(5*rand.random(), 3)
-  cursor_FT2.execute("INSERT INTO dbo.km_t([UNOS_ID], [time_stamp], [kidney_mass]) VALUES('{}', GETDATE(), {});".format(unos_id, data_UO))
+  cursor_FT2.execute("INSERT INTO dbo.uo_t([UNOS_ID], [time_stamp], [urine_output]) VALUES('{}', GETDATE(), {});".format(unos_id, data_UO))
   cnxn_FT2.commit()
   #Biotrend data
-  data_sO2 = round(100*rand.random(), 3)
+  data_sO2v = round(100*rand.random(), 3)
   data_hct = round(30*rand.random(), 3)
   cursor_BT.execute("INSERT INTO dbo.bt_t([UNOS_ID], [time_stamp], [sO2], [hct]) VALUES('{}', GETDATE(), {}, {});".format(unos_id, data_sO2v, data_hct))
   cnxn_BT.commit()
