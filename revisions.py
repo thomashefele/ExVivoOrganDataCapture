1676de53c88c0f919e2f79e6f5cc1172f527a977
Thomas Hefele <116929892+thomashefele@users.noreply.github.com>
2023-03-07 16:21:00 -0500

    Update revisions.py

--- revisions.py
+++ revisions.py
@@ -743,35 +743,35 @@ def app(UNOS_AGAIN= None):
                                     if isinstance(don_row[i], float):
                                         if np.isnan(don_row[i]):
                                             don_row[i] = None
-                                try:
-                                    with pyodbc.connect(connString) as cnxn_DI:
-                                        with cnxn_DI.cursor() as cursor:                                
-                                            exec_str_1 = "INSERT INTO dbo.organ([{0}],[{1}],[{2}],[{3}],[{4}],[{5}],[{6}],[{7}],[{8}],[{9}],"
-                                            exec_str_2 = "[{10}],[{11}],[{12}],[{13}],[{14}],[{15}],[{16}],[{17}],[{18}],[{19}],[{20}],[{21}],[{22}],"
-                                            exec_str_3 = "[{23}],[{24}],[{25}],[{26}],[{27}],[{28}],[{29}],[{30}],[{31}],[{32}],[{33}],[{34}],[{35}],[{36}],[{37}],"
-                                            exec_str_4 = "[{38}],[{39}],[{40}],[{41}],[{42}],[{43}],[{44}],[{45}],[{46}],[{47}],[{48}],[{49}],[{50}],[{51}],[{52}],"
-                                            exec_str_5 = "[{53}],[{54}],[{55}],[{56}]) "
-                                            exec_str_6 = "VALUES('{57}','{58}','{59}','{60}','{61}','{62}','{63}','{64}','{65}','{66}','{67}','{68}','{69}','{70}',"
-                                            exec_str_7 = "'{71}','{72}','{73}','{74}','{75}','{76}','{77}','{78}','{80}','{81}','{82}','{83}','{84}','{85}','{86}',"
-                                            exec_str_8 = "'{87}','{88}','{89}','{90}','{91}','{92}','{93}','{94}','{95}','{96}','{97}','{98}','{99}','{100}','{101}',"
-                                            exec_str_9 = "'{102}','{103}','{104}','{105}','{106}','{107}','{108}','{109}','{110}','{111}','{112}','{113}');"
-                                            execstr = exec_str_1+exec_str_2+exec_str_3+exec_str_4+exec_str_5+exec_str_6+exec_str_7+exec_str_8+exec_str_9
-                                            cursor.execute(execstr.format(*head_row, *don_row))
-                                            cnxn_DI.commit()       
-                                except (pyodbc.InterfaceError, pyodbc.OperationalError, pyodbc.ProgrammingError, pyodbc.IntegrityError, pyodbc.DataError, pyodbc.NotSupportedError):
-                                    upload_status = False
+                                #try:
+                                with pyodbc.connect(connString) as cnxn_DI:
+                                    with cnxn_DI.cursor() as cursor:                                
+                                        exec_str_1 = "INSERT INTO dbo.organ([{0}],[{1}],[{2}],[{3}],[{4}],[{5}],[{6}],[{7}],[{8}],[{9}],"
+                                        exec_str_2 = "[{10}],[{11}],[{12}],[{13}],[{14}],[{15}],[{16}],[{17}],[{18}],[{19}],[{20}],[{21}],[{22}],"
+                                        exec_str_3 = "[{23}],[{24}],[{25}],[{26}],[{27}],[{28}],[{29}],[{30}],[{31}],[{32}],[{33}],[{34}],[{35}],[{36}],[{37}],"
+                                        exec_str_4 = "[{38}],[{39}],[{40}],[{41}],[{42}],[{43}],[{44}],[{45}],[{46}],[{47}],[{48}],[{49}],[{50}],[{51}],[{52}],"
+                                        exec_str_5 = "[{53}],[{54}],[{55}],[{56}]) "
+                                        exec_str_6 = "VALUES('{57}','{58}','{59}','{60}','{61}','{62}','{63}','{64}','{65}','{66}','{67}','{68}','{69}','{70}',"
+                                        exec_str_7 = "'{71}','{72}','{73}','{74}','{75}','{76}','{77}','{78}','{80}','{81}','{82}','{83}','{84}','{85}','{86}',"
+                                        exec_str_8 = "'{87}','{88}','{89}','{90}','{91}','{92}','{93}','{94}','{95}','{96}','{97}','{98}','{99}','{100}','{101}',"
+                                        exec_str_9 = "'{102}','{103}','{104}','{105}','{106}','{107}','{108}','{109}','{110}','{111}','{112}','{113}');"
+                                        execstr = exec_str_1+exec_str_2+exec_str_3+exec_str_4+exec_str_5+exec_str_6+exec_str_7+exec_str_8+exec_str_9
+                                        cursor.execute(execstr.format(*head_row, *don_row))
+                                        cnxn_DI.commit()       
+                                #except (pyodbc.InterfaceError, pyodbc.OperationalError, pyodbc.ProgrammingError, pyodbc.IntegrityError, pyodbc.DataError, pyodbc.NotSupportedError):
+                                    #upload_status = False
                             except (KeyError, IndexError, pd.errors.InvalidIndexError):
                                 pass
                             
-                            try:
-                                file_write("{}_donor_info.csv".format(unos_ID), head_row, don_row)
-                            except(PermissionError, OSError, FileNotFoundError):
-                                file_status = False
+                            #try:
+                            file_write("{}_donor_info.csv".format(unos_ID), head_row, don_row)
+                            #except(PermissionError, OSError, FileNotFoundError):
+                                #file_status = False
                                 
-                            if upload_status == True or file_status == True:
-                                Label(data_w, text= "Data successfully saved!", font= txt, padx= 5).place(relx= 0.6, rely= 0.95, anchor= CENTER)
-                            else:
-                                Label(data_w, text= "Data saving unsuccessful.", font= txt, padx= 5).place(relx= 0.6, rely= 0.95, anchor= CENTER)
+                            #if upload_status == True or file_status == True:
+                             #   Label(data_w, text= "Data successfully saved!", font= txt, padx= 5).place(relx= 0.6, rely= 0.95, anchor= CENTER)
+                            #else:
+                             #   Label(data_w, text= "Data saving unsuccessful.", font= txt, padx= 5).place(relx= 0.6, rely= 0.95, anchor= CENTER)
 
                         if no_fitz == False:
                             donor_file = find("{}.pdf".format(unos_ID), "/")
